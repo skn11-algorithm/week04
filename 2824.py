bcf917c0582c1c629b3908efbe9f5b821229bb31
Ohjunghh <cmt5ttt@gmail.com>
2025-03-16 02:20:32 +0900

    [ojh] boj: complete 2824.py

--- 2824.py
+++ 2824.py
@@ -1,21 +1,20 @@
 import sys
+sys.setrecursionlimit(10**6) #Python이 정한 최대 재귀 갚이를 변경 가능
 input=sys.stdin.readline
 
 def gcd(A,B):
-    if B==0: #만약, 9자리보다 길다면, 마지막 9자리만 출력
-        if A>=1000000000:
-            A=A%1000000000
-            print("%09d"%A)
-        else:
-            print(A)
-        exit()
-    gcd(B,A%B)
+    if B==0:
+        return A
+    return gcd(B,A%B)
 
-nums=[1,1]
-for i in range(2):
-    input()
-    A=list(map(int,input().split()))
-    for a in A:
-        nums[i]*=a
-nums.sort()
-gcd(nums[1],nums[0])
+
+a,b=1,1
+input()
+A=input().split()
+for i in A : a*=int(i)
+input()
+B=input().split()
+for i in B : b*=int(i)
+
+a,b= (a,b) if a>b else (b,a)
+print(str(gcd(a,b))[-9:]) #만약, 9자리보다 길다면, 마지막 9자리만 출력
