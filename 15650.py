2e7056a0653cb56ff14a52ee89e4035407eabe4a
Ohjunghh <cmt5ttt@gmail.com>
2025-03-16 01:00:54 +0900

    [ojh] boj: complete 15650.py

--- 15650.py
+++ 15650.py
@@ -3,17 +3,14 @@ input=sys.stdin.readline
 
 num,length=map(int,input().rstrip().split())
 stack=[]
-ck=[False]*(num+1)
-def nm(stack):
+
+def nm(stack,start):
     if len(stack)==length:
         print(*stack)
         return
-    for i in range(1,num+1):
-        if not ck[i]:
-            stack.append(i)
-            ck[i]=True
-            nm(stack)
-            stack.pop()
-            ck[i]=False
+    for i in range(start,num+1): #이전 숫자보다 작은 숫자는 제외
+        stack.append(i)
+        nm(stack,i+1)
+        stack.pop()
 
-nm(stack)
+nm(stack,1)
